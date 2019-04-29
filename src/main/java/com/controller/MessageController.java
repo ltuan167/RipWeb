e34dda74e8691071ce4dd4d38ec2b2fa3ce01bc6
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2019-04-29 22:41:42 +0700

    Add diff /topic for websocket

--- src/main/java/com/controller/MessageController.java
+++ src/main/java/com/controller/MessageController.java
@@ -3,12 +3,9 @@ package com.controller;
 import com.model.Greeting;
 import com.model.HelloMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
@@ -21,14 +18,14 @@ public class MessageController {
 	@MessageMapping("/hello")
 //	@SendTo("/topic/greetings")
 	public Greeting greeting(HelloMessage message, SimpMessageHeaderAccessor headerAccessor) throws Exception {
-//		String sessionId = headerAccessor.getSessionAttributes().get("sessionId").toString();
-		String sessionId = headerAccessor.getSessionId();
-//		headerAccessor.getSessionAttributes()
-//		String sessionId = headerAccessor.getSessionId();
-		System.out.println(sessionId + ": " + message.getName());
-		Greeting greeting = new Greeting("Hellooooooo, " + HtmlUtils.htmlEscape(message.getName()) + "!");
-		messagingTemplate.convertAndSend("/topic/greetings", greeting);
-//		System.out.println(headerAccessor.getUser().getName());
+		String sessionId = headerAccessor.getSessionAttributes().get("sessionId").toString();
+		String echoMessage = message.getName();
+
+		System.out.println(String.format("[WS][RECEIVED][FROM %s]: %s", sessionId, echoMessage));
+
+		Greeting greeting = new Greeting("Hellooooooo, " + HtmlUtils.htmlEscape(echoMessage) + "!");
+
+		messagingTemplate.convertAndSend("/topic/" + sessionId, greeting);
 		return greeting;
 	}
 
