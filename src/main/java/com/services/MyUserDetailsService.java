8a44c4a521f465ccfee6926c7217abd52b721824
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2019-05-15 00:49:23 +0700

    Ignore built files

--- src/main/java/com/services/MyUserDetailsService.java
+++ src/main/java/com/services/MyUserDetailsService.java
@@ -1,10 +1,5 @@
 package com.services;
 
-import com.dao.UserDAO;
-import com.entities.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
