6b172e2732b3e38a0dc15d299bb7ec372fc64ee5
Khoa Tran <tranduckhoatcu@gmail.com>
2019-06-02 22:59:45 +0700

    add ButtonController

---
+++ src/main/java/com/controller/ButtonController.java
@@ -0,0 +1,24 @@
+package com.controller;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ButtonController {
+
+    //    @Secured("ROLE_USER")
+    @GetMapping("/host")
+    public String homepage() { return "hostView";}
+
+    @GetMapping("/editOverview")
+    public String editview() {return "editOverview";}
+
+    @GetMapping("/editQuestion")
+    public String editquestion() {return "editQuestion";}
+
+    @GetMapping("/displayQuestion")
+    public String displayQuestion() {return "displayQuestion";}
+
+}
+
