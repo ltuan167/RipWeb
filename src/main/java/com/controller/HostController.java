6b35649cd8113866f52e46dc83eb678b63096d50
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2019-06-03 15:44:15 +0700

    Update

--- src/main/java/com/controller/HostController.java
+++ src/main/java/com/controller/HostController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ButtonController {
+public class HostController {
 
     @GetMapping("/host")
     public String homepage() { return "hostView";}
